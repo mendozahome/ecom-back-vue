6aa2436bfd26e9b56164149be226f5ccc3aad2ba
mendozahome <supermerolul@gmail.com>
2023-06-19 23:33:57 -0600

    second

--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 import express from "express"
+import { cartItems, products } from './../temp-data'
 
 const app = express()
 
 app.get('/hello', (req, res) =>{
     res.send('hello!');
+});
+
+app.get('/products', (req, res) =>{
+    res.json(products)
 })
+
+app.get('/cart', (req, res) =>{
+    res.json(cartItems)
+})
+
+app.get('/products/:productId', (req, res) =>{
+    const productId = req.params.productId;
+    const product = products.find(product => product.id === productId);
+    res.json(product)
+})
+
 
 app.listen(8000, () => {
     console.log('server is listening on port 8000')
