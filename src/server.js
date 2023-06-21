fa877ef2ab1af96c91103544f5a3fb84780a0948
mendozahome <supermerolul@gmail.com>
2023-06-21 17:19:48 -0400

    added assets folder to backend

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 import express from "express"
-import { cartItems as cartItemsRaw, products as productsRaw } from './../temp-data'
 import cors from "cors"
 import { connectToDb, getDb } from "./index";
+import path from 'path';
 
 require('dotenv').config()
 
 
-let cartItems = cartItemsRaw;
-let products = productsRaw;
 
 // db connection 
 let db
@@ -25,11 +23,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use('/images', express.static(path.join(__dirname, '../assets')) )
 
 app.get('/hello', (req, res) =>{
     });
 
-app.get('/products', (req, res) =>{
+app.get('/api/products', (req, res) =>{
     let products = [];
     db.collection(process.env.MONGO_COLLECTION)
       .find()   // cursor toArray forEach
@@ -47,19 +46,19 @@ async function populateCartIds(ids){
     return Promise.all(ids.map(id => db.collection(process.env.MONGO_COLLECTION).findOne({ id })) )
 }
 
-app.get('/users/:userId/cart', async (req, res) =>{
+app.get('/api/users/:userId/cart', async (req, res) =>{
     const user = await db.collection('users')
         .findOne({ id: req.params.userId})
     const populatedCart = await populateCartIds(user.cartItems)
         res.json(populatedCart)
 })
 
-app.get('/products/:productId',async (req, res) =>{
+app.get('/api/products/:productId',async (req, res) =>{
     const productId = req.params.productId;
     const product = await db.collection(process.env.MONGO_COLLECTION).findOne({ id: productId});
         res.json(product)
 })
-app.post('/users/:userId/cart', async (req, res) => {
+app.post('/api/users/:userId/cart', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.body.id;
 
@@ -72,7 +71,7 @@ app.post('/users/:userId/cart', async (req, res) => {
         res.json(populatedCart);
 })
 
-app.delete('/users/:userId/cart/:productId',async (req,res) =>{
+app.delete('/api/users/:userId/cart/:productId',async (req,res) =>{
     const userId = req.params.userId;
     const productId = req.params.productId;
 
